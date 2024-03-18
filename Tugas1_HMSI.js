ba02dee7c6ae7575d5c3b790d64b02c11e0a2ee7
dikaadlisaputra <162800901+dikaadlisaputra@users.noreply.github.com>
2024-03-18 14:09:46 +0700

    Add files via upload

---
+++ Tugas1_HMSI.js
@@ -0,0 +1,13 @@
+// Buat variabel identitas dengan tipe object
+let identitas = {
+  nama: "Dika Adli Saputra",
+  kota: "Tanggerang",
+  asalSekolah: "SMK 1 Kab. Tanggerang ",
+  jurusan: "TKJ",
+  sedangBekerja: false,
+};
+
+// Buat variabel nama dan isi dengan nilai nama dari properti object identitas
+let nama = identitas.nama;
+
+console.log(nama); // Output: Dika Adli Saputra
